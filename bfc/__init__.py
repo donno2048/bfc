4fa0c6884a699b181fe684008ab72b971f4fd495
donno2048 <just4now666666@gmail.com>
2024-01-18 21:21:07 +0000

    fix issue

--- bfc/__init__.py
+++ bfc/__init__.py
@@ -5,17 +5,17 @@ def start_loop() -> str:
     global labels, label_count
     label_count += 1
     labels.append(label_count)
-    return f"loop_start{label_count}:\nldr x3, [x1]\ncmp x3, #0\nbeq loop_end{label_count}\n"
+    return f"loop_start{label_count}:\nldrb w3, [x1]\ncmp w3, wzr\nbeq loop_end{label_count}\n"
 def end_loop() -> str:
     global labels
     label_count = labels.pop()
     return f"b loop_start{label_count}\nloop_end{label_count}:\n"
-initialization = ".data\ntape: .fill 30000\nend:\n.text\n.global _start\n_start:\nldr x1, =tape\nmov x2, #1\nldr x4, =tape\nldr x5, =end-1\n"
+initialization = ".data\ntape: .fill 30000\nend:\n.text\n.global _start\n_start:\nldr x1, =tape\nmov x2, #1\nmov x4, x1\nldr x5, =end-1\n"
 conversion = {
     '>': lambda: "cmp x1, x5\ncsinc x1, x4, x1, EQ\n",
     '<': lambda: "cmp x1, x4\ncsinc x1, x1, x5, NE\nsub x1, x1, x2\n",
-    '+': lambda: "ldr x3, [x1]\nadd x3, x3, x2\nstr x3, [x1]\n",
-    '-': lambda: "ldr x3, [x1]\nsub x3, x3, x2\nstr x3, [x1]\n",
+    '+': lambda: "ldrb w3, [x1]\nadd w3, w3, #1\nstrb w3, [x1]\n",
+    '-': lambda: "ldrb w3, [x1]\nsub w3, w3, #1\nstrb w3, [x1]\n",
     '.': lambda: "mov x0, #1\nmov w8, #64\nsvc #0\n",
     ',': lambda: "mov x0, #0\nmov w8, #63\nsvc #0\n",
     '[': start_loop,
